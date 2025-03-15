9855fd03a4311e1910a941cce1b5272504ac00e8
unknown <jhonas.goncalves21@gmail.com>
2025-03-14 21:35:41 -0300

    Primeiro Commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,12 @@
+import { Lusitana } from 'next/font/google';
+import { Inter } from 'next/font/google';
+
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: ['400', '700'], // Specify the weights you want to use
+});
+
+export const inter = Inter({
+    subsets: ['latin'],
+    weight: ['400', '700'], // Specify the weights you want to use
+});
